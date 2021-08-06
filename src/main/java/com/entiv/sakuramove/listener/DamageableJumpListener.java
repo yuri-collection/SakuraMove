b562164825cd9c3ccc4b10647d2e254c2078d035
EnTIv <1522935501@qq.com>
2021-08-06 11:25:40 +0800

    传一个类

---
+++ src/main/java/com/entiv/sakuramove/listener/DamageableJumpListener.java
@@ -0,0 +1,32 @@
+package com.entiv.sakuramove.listener;
+
+import com.destroystokyo.paper.event.player.PlayerJumpEvent;
+import com.entiv.sakuramove.action.DamageableJump;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.*;
+
+public class DamageableJumpListener implements Listener {
+
+    private final DamageableJump doubleJump = DamageableJump.getInstance();
+
+    @EventHandler
+    public void onDoubleJump(PlayerToggleFlightEvent event) {
+        Player player = event.getPlayer();
+
+        if (player.getGameMode() == GameMode.CREATIVE || player.getGameMode() == GameMode.SPECTATOR || player.hasPermission("sakuramove.fly")) {
+            return;
+        }
+
+        doubleJump.accept(player);
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPlayerJump(PlayerJumpEvent event) {
+        Player player = event.getPlayer();
+        doubleJump.enable(player);
+    }
+}
